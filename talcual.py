5f3a863b346b36651483bf3583acab9ee756c456
josprimen <36623141+josprimen@users.noreply.github.com>
2019-10-11 11:35:26 +0200

    The hole example done. The code is not mine.

---
+++ talcual.py
@@ -0,0 +1,107 @@
+import numpy
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+
+
+
+
+# fix random seed for reproducibility
+numpy.random.seed(7)
+
+# load the dataset
+dataframe = pandas.read_csv('airline-passengers.csv', usecols=[1], engine='python')
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+
+print('Dataset: ' + str(dataset))
+
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+
+print('Dataset normalizado: ' + str(dataset))
+
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
+print(len(train), len(test))
+print('entrenamiento: ' + str(train))
+print('test: ' + str(test))
+
+# convert an array of values into a dataset matrix
+def create_dataset(dataset, look_back=1):
+	dataX, dataY = [], []
+	for i in range(len(dataset)-look_back-1):
+		a = dataset[i:(i+look_back), 0]
+		dataX.append(a)
+		dataY.append(dataset[i + look_back, 0])
+	return numpy.array(dataX), numpy.array(dataY)
+
+
+# reshape into X=t and Y=t+1
+look_back = 1
+trainX, trainY = create_dataset(train, look_back)
+testX, testY = create_dataset(test, look_back)
+
+print('trainX: ' + str(trainX))
+print('trainY: ' + str(trainY))
+print('testX: ' + str(testX))
+print('testY: ' + str(testY))
+
+# reshape input to be [samples, time steps, features]
+trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+
+print('reshape entrenamientox : ' + str(trainX))
+print('reshape testx: ' + str(testX))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(1, look_back)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+print('Datos que va a usar la red neuronal: \n')
+print('trainX: \n' )
+print(trainX)
+print('trainY: \n')
+print(trainY)
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+
+# make predictions
+trainPredict = model.predict(trainX)
+print('La prediccion de la red neuronal para los datos de entrenamiento: \n' + str(trainPredict))
+testPredict = model.predict(testX)
+print('La prediccion de la red neuronal para los datos de test: \n' + str(testPredict))
+# invert predictions
+trainPredict = scaler.inverse_transform(trainPredict)
+print('La prediccion de la red neuronal para los datos de entrenamiento INVERTIDOS: \n' + str(trainPredict))
+trainY = scaler.inverse_transform([trainY])
+testPredict = scaler.inverse_transform(testPredict)
+print('La prediccion de la red neuronal para los datos de test INVERTLDOS: \n' + str(testPredict))
+testY = scaler.inverse_transform([testY])
+# calculate root mean squared error
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+print('Train Score: %.2f RMSE' % (trainScore))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f RMSE' % (testScore))
+
+# shift train predictions for plotting
+trainPredictPlot = numpy.empty_like(dataset)
+trainPredictPlot[:, :] = numpy.nan
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+# shift test predictions for plotting
+testPredictPlot = numpy.empty_like(dataset)
+testPredictPlot[:, :] = numpy.nan
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+# plot baseline and predictions
+plt.plot(scaler.inverse_transform(dataset))
+plt.plot(trainPredictPlot)
+plt.plot(testPredictPlot)
+plt.show()
