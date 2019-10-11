fd38f07973b87650183777e925986f0973904ecc
josprimen <36623141+josprimen@users.noreply.github.com>
2019-10-11 13:48:17 +0200

    Example done with my own code. I did the data preparation in another way.

--- talcual.py
+++ talcual.py
@@ -39,6 +39,7 @@ print('test: ' + str(test))
 def create_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
+		print(i)
 		a = dataset[i:(i+look_back), 0]
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
