bddcc337d4157fcce0dd8ff1d72390d10bcafade
josprimen <36623141+josprimen@users.noreply.github.com>
2019-11-25 18:17:28 +0100

    Final version

--- airlines_example.py
+++ airlines_example.py
@@ -8,113 +8,99 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-#Esto para representar los datos en una gráfica
-#conjunto_datos = pandas.read_csv('airline-passengers.csv', usecols=[1], engine='python')
-#plt.plot(conjunto_datos)
-#plt.show()
 
-#Para hacer que siempre salgan los mismos número aleatorios
+#Set random seed to make initial weights static.
 np.random.seed(7)
 
-#Vamos a reusar el código de representación de datos para extraer los datos del csv
-datos = pandas.read_csv('airline-passengers.csv', usecols=[1], engine='python')
-conjunto = datos.values
-#pasamos los números a punto flotante porque las redes neuronales funcionan mejor con este tipo de datos
-conjunto = conjunto.astype('float32')
+#Load and represent the dataset
+data = pandas.read_csv('airline-passengers.csv', usecols=[1], engine='python')
+plt.plot(data)
+plt.show()
+dataset = data.values
+dataset = dataset.astype('float32')
 
-#Las redes LSTM son sensible a cómo estén escalados los datos
-#Funcionan mejor con datos de 0 a 1 y más al usar sigmoide de activación
-#Normalizamos. Para ello creamos una variable con el rango en el que vamos a normalizar y normalizamos en ese rango
+#Normalize the dataset
 minmax = MinMaxScaler(feature_range=(0,1))
-#max(conjunto) = 622, min(conjunto)=104.  112-104/622-104 = 0.015
-conjunto = minmax.fit_transform(conjunto)
-print('El conjunto normalizado: \n' + str(conjunto))
+dataset = minmax.fit_transform(dataset)
 
-#Como hemos de tener un conjunto de entrenamiento y uno de test, vamos a separar el que tenemos en dos
-#un 67% de los datos los usaremos para entrenar y el resto para el conjunto de prueba
-tamaño_entrenamiento = int(len(conjunto) * 0.67)
-tamaño_test = len(conjunto) - tamaño_entrenamiento
-entrenamiento = conjunto[0:tamaño_entrenamiento]
-test = conjunto[tamaño_entrenamiento:len(conjunto)]
+#Split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train = dataset[0:train_size]
+test = dataset[train_size:len(dataset)]
 
-print('Tamaño cojunto entrenamiento: ' +
-      str(len(entrenamiento)), 'Tamaño conjunto pruebas: ' +
-      str(len(test)), 'Tamaño total conjunto: ' +str(len(conjunto)))
-print('Conjunto entrenamiento: \n' + str(entrenamiento))
-print('Conjunto pruebas: \n' + str(test))
-
-
-#Creamos los conjuntos de dato con el formato adecuado para LSTM
-
-def datosX (conjunto):
+#Define a function to format the dataset
+def dataX (dataset, look_back):
     aux = []
-    for i in range (len(conjunto)-2):
-        a = [conjunto[i]]
+    for i in range (0,(len(dataset)-(look_back+1))):
+        a = []
+        for n in range (look_back):
+            a.append(dataset[i+n])
         aux.append(a)
     return np.array(aux)
 
-def datosY (conjunto):
+def dataY (dataset, look_back):
     aux = []
-    for i in range (len(conjunto)-2):
-        aux.append(conjunto[i+1,0])
+    for i in range ((look_back-1),(len(dataset)-2)):
+        aux.append(dataset[i+1,0])
     return np.array(aux)
 
+#Format dataset
+look_back = 5 #time steps
+trainX = dataX(train, look_back)
+trainY = dataY(train, look_back)
 
-entrenamientoX = datosX(entrenamiento)
-entrenamientoY = datosY(entrenamiento)
-
-pruebasX = datosX(test)
-pruebasY = datosY(test)
-
-
-
-print('Conjunto de entrenamiento t mio bien: \n' + str(entrenamientoX))
-print('Con shape')
-print(entrenamientoX.shape)
-print('Conjunto de entrenamiento t+1 mio: \n' + str(entrenamientoY))
-print('Conjunto prueba t: \n' + str(pruebasX))
-print('Conjunto prueba t+1: \n' + str(pruebasY))
+testX = dataX(test, look_back)
+testY = dataY(test, look_back)
 
 
 
-# creamos y entrenamos la red LSTM
-paso_atras = 1 #de cúantas unidades de tiempo son los pasos
+print('Conjunto de entrenamientoX t: \n' + str(trainX))
+print('Con shape' + str(trainX.shape))
+print('Conjunto de entrenamientoY t+1: \n' + str(trainY))
+print('Con shape' + str(trainY.shape))
+print('Conjunto pruebaX t: \n' + str(testX))
+print('Conjunto pruebaY t+1: \n' + str(testY))
+
+
+
+#Create and fit the LSTM network
 model = Sequential()
-model.add(LSTM(4, input_shape=(1, paso_atras)))
+model.add(LSTM(4, input_shape=(look_back,1)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
 
-model.fit(entrenamientoX, entrenamientoY, epochs=100, batch_size=1, verbose=2)
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 
 
-# Hacemos la predicción
-trainPredict = model.predict(entrenamientoX)
-print('La prediccion de la red neuronal para los datos de entrenamiento: \n' + str(trainPredict))
-testPredict = model.predict(pruebasX)
-print('La prediccion de la red neuronal para los datos de test: \n' + str(testPredict))
-# Invertir el normalizado para obtener los datos de pasajeros en escala unidades de 1000
+#Make prediction
+trainPredict = model.predict(trainX)
+print('La predicción de la red neuronal para los datos de entrenamiento: \n' + str(trainPredict))
+testPredict = model.predict(testX)
+print('La predicción de la red neuronal para los datos de test: \n' + str(testPredict))
+#Invert the normalize
 trainPredict = minmax.inverse_transform(trainPredict)
-print('La prediccion de la red neuronal para los datos de entrenamiento INVERTIDOS: \n' + str(trainPredict))
-trainY = minmax.inverse_transform([entrenamientoY])
+print('La predicción de la red neuronal para los datos de entrenamiento INVERTIDOS: \n' + str(trainPredict))
+trainY = minmax.inverse_transform([trainY])
 testPredict = minmax.inverse_transform(testPredict)
-print('La prediccion de la red neuronal para los datos de test INVERTLDOS: \n' + str(testPredict))
-testY = minmax.inverse_transform([pruebasY])
-# Calculamos el error cuadrático medio
+print('La predicción de la red neuronal para los datos de test INVERTLDOS: \n' + str(testPredict))
+testY = minmax.inverse_transform([testY])
+#Calculate the mean square error
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
-# shift train predictions for plotting
-trainPredictPlot = np.empty_like(conjunto)
+#Shift train predictions for plotting
+trainPredictPlot = np.empty_like(dataset)
 trainPredictPlot[:, :] = np.nan
-trainPredictPlot[paso_atras:len(trainPredict)+paso_atras, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = np.empty_like(conjunto)
+trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
+#Shift test predictions for plotting
+testPredictPlot = np.empty_like(dataset)
 testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict)+(paso_atras*2)+1:len(conjunto)-1, :] = testPredict
-# plot baseline and predictions
-plt.plot(minmax.inverse_transform(conjunto))
+testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredict
+#Plot baseline and predictions
+plt.plot(minmax.inverse_transform(dataset))
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
 plt.show()
--- talcual.py
+++ talcual.py
@@ -1,132 +1,69 @@
+# LSTM for international airline passengers problem with time step regression framing
 import numpy
 import matplotlib.pyplot as plt
-import pandas
+from pandas import read_csv
 import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-
-
-
-
-# fix random seed for reproducibility
-numpy.random.seed(7)
-
-# load the dataset
-dataframe = pandas.read_csv('airline-passengers.csv', usecols=[1], engine='python')
-dataset = dataframe.values
-dataset = dataset.astype('float32')
-
-print('Dataset: ' + str(dataset))
-
-# normalize the dataset
-scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
-
-print('Dataset normalizado: ' + str(dataset))
-
-# split into train and test sets
-train_size = int(len(dataset) * 0.67)
-test_size = len(dataset) - train_size
-train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
-print(len(train), len(test))
-print('entrenamiento: ' + str(train))
-print('test: ' + str(test))
-
 # convert an array of values into a dataset matrix
 def create_dataset(dataset, look_back=1):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back-1):
-		print(i)
 		a = dataset[i:(i+look_back), 0]
 		dataX.append(a)
 		dataY.append(dataset[i + look_back, 0])
 	return numpy.array(dataX), numpy.array(dataY)
-
-
+# fix random seed for reproducibility
+numpy.random.seed(7)
+# load the dataset
+dataframe = read_csv('airline-passengers.csv', usecols=[1], engine='python')
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+# normalize the dataset
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
+# split into train and test sets
+train_size = int(len(dataset) * 0.67)
+test_size = len(dataset) - train_size
+train, test = dataset[0:train_size,:], dataset[train_size:len(dataset),:]
 # reshape into X=t and Y=t+1
-look_back = 3
+look_back = 5
 trainX, trainY = create_dataset(train, look_back)
 testX, testY = create_dataset(test, look_back)
-
-trainX_timestep, trainY_timestep = create_dataset(train, look_back)
-testX_timestep, testY_timestep = create_dataset(test, look_back)
-
-print('trainX: ' + str(trainX))
-print('trainY: ' + str(trainY))
-print('testX: ' + str(testX))
-print('testY: ' + str(testY))
-
 # reshape input to be [samples, time steps, features]
-trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+trainX = numpy.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = numpy.reshape(testX, (testX.shape[0], testX.shape[1], 1))
 
-print('reshape entrenamientox : ' + str(trainX))
-print('reshape testx: ' + str(testX))
 
-# reshape using time_step
-trainX_timestep = numpy.reshape(trainX_timestep, (trainX_timestep.shape[0], trainX_timestep.shape[1], 1))
-testX_timestep = numpy.reshape(testX_timestep, (testX_timestep.shape[0], testX_timestep.shape[1], 1))
-
-print('reshape entrenamientox time_step : ' + str(trainX_timestep))
+print('Conjunto de entrenamientoX t mio bien: \n' + str(trainX))
 print('Con shape')
-print(trainX_timestep.shape)
-print('reshape testx time_step: ' + str(testX_timestep))
+print(trainX.shape)
+print('Conjunto de entrenamientoY t+1 mio: \n' + str(trainY))
+print('Conjunto pruebaX t: \n' + str(testX))
+print('Conjunto pruebaY t+1: \n' + str(testY))
 
 # create and fit the LSTM network
 model = Sequential()
-#model.add(LSTM(4, input_shape=(look_back,1)))
-
-# first model layer code with memory
-batch_size = 1
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True, return_sequences=True))
-model.add(LSTM(4, batch_input_shape=(batch_size, look_back, 1), stateful=True))
-
+model.add(LSTM(4, input_shape=(look_back, 1)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-print('Datos que va a usar la red neuronal: \n')
-print('trainX: \n')
-#print(trainX)
-print(trainX_timestep)
-print('trainY: \n')
-#print(trainY)
-print(trainY_timestep)
-#model.fit(trainX_timestep, trainY_timestep, epochs=100, batch_size=1, verbose=2)
-
-# fit code with memory
-for i in range(300):
-    model.fit(trainX_timestep, trainY_timestep, epochs=1, batch_size=batch_size, verbose=2, shuffle=False)
-    model.reset_states()
-
+model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
 # make predictions
-#trainPredict = model.predict(trainX_timestep)
-#print('La prediccion de la red neuronal para los datos de entrenamiento: \n' + str(trainPredict))
-#testPredict = model.predict(testX_timestep)
-#print('La prediccion de la red neuronal para los datos de test: \n' + str(testPredict))
-
-# make predictions with memory
-trainPredict = model.predict(trainX_timestep, batch_size=batch_size)
-print('La prediccion de la red neuronal para los datos de entrenamiento: \n' + str(trainPredict))
-model.reset_states()
-testPredict = model.predict(testX_timestep, batch_size=batch_size)
-print('La prediccion de la red neuronal para los datos de test: \n' + str(testPredict))
-
-
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
 # invert predictions
 trainPredict = scaler.inverse_transform(trainPredict)
-print('La prediccion de la red neuronal para los datos de entrenamiento INVERTIDOS: \n' + str(trainPredict))
-trainY_timestep = scaler.inverse_transform([trainY_timestep])
+trainY = scaler.inverse_transform([trainY])
 testPredict = scaler.inverse_transform(testPredict)
-print('La prediccion de la red neuronal para los datos de test INVERTLDOS: \n' + str(testPredict))
-testY_timestep = scaler.inverse_transform([testY_timestep])
+testY = scaler.inverse_transform([testY])
 # calculate root mean squared error
-trainScore = math.sqrt(mean_squared_error(trainY_timestep[0], trainPredict[:, 0]))
+trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
-testScore = math.sqrt(mean_squared_error(testY_timestep[0], testPredict[:, 0]))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
-
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
