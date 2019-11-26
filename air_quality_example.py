419510da504f679df1eb54aa0d3ca93c13abc2bd
josprimen <36623141+josprimen@users.noreply.github.com>
2019-11-26 11:59:42 +0100

    Final version

--- air_quality_example.py
+++ air_quality_example.py
@@ -16,44 +16,38 @@ from keras.layers import LSTM
 #Set random seed to make initial weights static.
 np.random.seed(7)
 
-datos = read_csv('raw.csv', usecols=[5, 6, 7, 8, 9, 10, 11, 12], engine='python')
-datos['pm2.5'].fillna(0, inplace=True)
-datos = datos[24:]
-#datos.to_csv('datosmios.csv')
-groups = [0, 1, 2, 3, 5, 6, 7]
-columns = ['pollution', 'dew', 'temp', 'press', 'wnd_spd', 'snow', 'rain']
-datoss = datos.values
+#Load and represent the dataset
 
-#Imprimimos una gráfica con los datos por pantalla (cada tipo de dato en un subgráfico)
+data = read_csv('raw.csv', usecols=[5, 6, 7, 8, 9, 10, 11, 12], engine='python')
+data['pm2.5'].fillna(0, inplace=True)
+data = data[24:]
+groups = [0, 1, 2, 3, 5, 6, 7]
+#columns = ['pollution', 'dew', 'temp', 'press', 'wnd_spd', 'snow', 'rain']
+dataset = data.values
+features = 8
+
 aux = 1
 pyplot.figure()
 for group in groups:
     pyplot.subplot(7, 1, aux)
-    pyplot.plot(datoss[:, group])
-    pyplot.title(columns[aux-1], y=0.5, loc='right')
-
+    pyplot.plot(dataset[:, group])
+    #pyplot.title(columns[aux-1], y=0.5, loc='right')
     aux = aux+1
-
 pyplot.show()
 
-#Encodeamos los datos de la dirección del viento para que tome un valor entero en lugar de ser categórico
+#Encode and normalize data
 encoder = LabelEncoder()
-datoss[:, 4] = encoder.fit_transform(datoss[:, 4])
-
-#Pasamos todos los datos a float y los normalizamos
-datoss = datoss.astype('float32')
+dataset[:, 4] = encoder.fit_transform(dataset[:, 4])
+dataset = dataset.astype('float32')
 scaler = MinMaxScaler(feature_range=(0, 1))
-datos_normalizados = scaler.fit_transform(datoss)
-#datos_normalizados.shape[1]
+normalize_data = scaler.fit_transform(dataset)
 
-#df = DataFrame(datos_normalizados)
-#len(df[0])
-#print('df[0][0]')
+#Define a function to format the dataset
 
-def datosX (conjunto, features):
-    df = DataFrame(conjunto)
+def dataX (dataset, features):
+    df = DataFrame(dataset)
     aux = []
-    for i in range (len(conjunto)-1):
+    for i in range (len(dataset)-1):
         a = []
         for n in range (features):
             a.append(df[n][i])
@@ -61,93 +55,78 @@ def datosX (conjunto, features):
         aux.append([a])
     return np.array(aux)
 
-def datosY (conjunto):
-    df = DataFrame(conjunto)
+def dataY (dataset):
+    df = DataFrame(dataset)
     aux = []
-    for i in range (len(conjunto)-1):
+    for i in range (len(dataset)-1):
         aux.append(df[0][i+1])
     return np.array(aux)
 
-#Un año de entrenamiento
-numero_horas_entrenamiento = 365*24
-entrenamiento = datos_normalizados[:numero_horas_entrenamiento, :]
-test = datos_normalizados[numero_horas_entrenamiento:, :]
+#Split into train and test sets
+training_set_hours = 365*24
+train = normalize_data[:training_set_hours, :]
+test = normalize_data[training_set_hours:, :]
+trainX, trainY = dataX(train, features), dataY(train)
+testX, testY = dataX(test, features), dataY(test)
 
-#test = datos_normalizados[:numero_horas_entrenamiento, :]
-#entrenamiento = datos_normalizados[numero_horas_entrenamiento:, :]
-
-print('Conjunto de entrenamiento')
-print(entrenamiento)
-
-features = 8
-entrenamientoX, entrenamientoY = datosX(entrenamiento, features), datosY(entrenamiento)
-testX, testY = datosX(test, features), datosY(test)
-
-print('DATOS entrenamientoX')
-print(entrenamientoX)
-print(len(entrenamientoX))
-print('DATOS entrenamientoY')
-print(entrenamientoY)
-print(len(entrenamientoY))
-print('DATOS testX')
+print('train dataset')
+print(train)
+print('data trainX')
+print(trainX)
+print(len(trainX))
+print('data trainY')
+print(trainY)
+print(len(trainY))
+print('data testX')
 print(testX)
 print(len(testX))
-print('DATOS testY')
+print('data testY')
 print(testY)
 print(len(testY))
 
-
+#Create and fit the LSTM network
 model = Sequential()
 model.add(LSTM(50, input_shape=(1, 8)))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
-
-# fit network
-history = model.fit(entrenamientoX, entrenamientoY, epochs=50, batch_size=72, validation_data=(testX, testY), verbose=2, shuffle=False)
-# plot history
+#Fit network
+history = model.fit(trainX, trainY, epochs=50, batch_size=72, validation_data=(testX, testY), verbose=2, shuffle=False)
+#Plot history
 pyplot.plot(history.history['loss'], label='train')
 pyplot.plot(history.history['val_loss'], label='test')
 pyplot.legend()
 pyplot.show()
 
+#Make prediction
+prediction_train = model.predict(trainX)
+print('DATO prediction_train')
+print(prediction_train)
+prediction_test = model.predict(testX)
+print('DATO prediction_test')
+print(prediction_test)
 
-#Hacer predicciones
-prediccion_entrenamiento = model.predict(entrenamientoX)
-print('DATO prediccion_entrenamiento')
-print(prediccion_entrenamiento)
-prediccion_test = model.predict(testX)
-print('DATO prediccion_test')
-print(prediccion_test)
-
-#Invertir el normalizado para tener los datos en la escala original
-#1 Hacer el array del mismo tamaño que el de la salida para poder concatenar
-entrenamientoX = entrenamientoX.reshape((entrenamientoX.shape[0], entrenamientoX.shape[2]))
-entrenamientoY = entrenamientoY.reshape((len(entrenamientoY), 1))
+#Format for concat
+trainX = trainX.reshape((trainX.shape[0], trainX.shape[2]))
+trainY = trainY.reshape((len(trainY), 1))
 testX = testX.reshape((testX.shape[0], testX.shape[2]))
 testY = testY.reshape((len(testY), 1))
-#2 Concatenar
-concatenado_entrenamiento_real = concatenate((entrenamientoY, entrenamientoX[:, 1:]), axis=1)
-concatenado_entrenamiento_prediccion = concatenate((prediccion_entrenamiento, entrenamientoX[:, 1:]), axis=1)
-concatenado_test_real = concatenate((testY, testX[:, 1:]), axis=1)
-concatenado_test_prediccion = concatenate((prediccion_test, testX[:, 1:]), axis=1)
-#3 Invertir el normalizado
-inversion_entrenamiento_real = scaler.inverse_transform(concatenado_entrenamiento_real)
-inversion_entrenamiento_prediccion = scaler.inverse_transform(concatenado_entrenamiento_prediccion)
-inversion_test_real = scaler.inverse_transform(concatenado_test_real)
-inversion_test_prediccion = scaler.inverse_transform(concatenado_test_prediccion)
-
-#4 Obtener las predicciones invertidas
-datos_real_entrenamiento = inversion_entrenamiento_real[:, 0]
-datos_prediccion_entrenamiento = inversion_entrenamiento_prediccion[:, 0]
-datos_real_test = inversion_test_real[:, 0]
-datos_prediccion_test = inversion_test_prediccion[:, 0]
-
-
-
-#Calcular el error cuadrático medio
-trainScore = sqrt(mean_squared_error(datos_real_entrenamiento, datos_prediccion_entrenamiento))
-testScore = sqrt(mean_squared_error(datos_real_test, datos_prediccion_test))
+#Concat
+concat_train_real = concatenate((trainY, trainX[:, 1:]), axis=1)
+concat_train_prediction = concatenate((prediction_train, trainX[:, 1:]), axis=1)
+concat_test_real = concatenate((testY, testX[:, 1:]), axis=1)
+concat_test_prediction = concatenate((prediction_test, testX[:, 1:]), axis=1)
+##Invert the normalize
+inversion_train_real = scaler.inverse_transform(concat_train_real)
+inversion_train_prediction = scaler.inverse_transform(concat_train_prediction)
+inversion_test_real = scaler.inverse_transform(concat_test_real)
+inversion_test_prediction = scaler.inverse_transform(concat_test_prediction)
+#Invert predictions
+data_real_train = inversion_train_real[:, 0]
+data_prediction_train = inversion_train_prediction[:, 0]
+data_real_test = inversion_test_real[:, 0]
+data_prediction_test = inversion_test_prediction[:, 0]
+##Calculate the mean square error
+trainScore = sqrt(mean_squared_error(data_real_train, data_prediction_train))
+testScore = sqrt(mean_squared_error(data_real_test, data_prediction_test))
 print('Train Score: %.2f RMSE' % (trainScore))
 print('Test Score: %.2f RMSE' % (testScore))
-
-
